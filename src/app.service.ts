377df79714703d9712bbba7e9dd0369dd84890f1
Юрий Аушев <wydo17@yandex.ua>
2021-03-31 16:01:16 +0300

    init

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return "<div id='root'>Hello World<div>";
+  }
+  getSecret(): string {
+    return 'вы раскрыли нашу тайну :(';
+  }
+}
