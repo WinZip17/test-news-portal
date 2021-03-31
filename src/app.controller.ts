0f6c5d00a67dcd4d073496a7ba8a5f2ae38d83f1
Юрий Аушев <wydo17@yandex.ua>
2021-03-31 17:18:30 +0300

    day 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Req, Post, Header} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Header('Access-Control-Allow-Origin', '*')
+
+  @Post()
+  create(): string {
+    return 'This action adds a new news';
+  }
 
   @Get()
-  getHello(): string {
+  getHello(@Req() request: Request): string {
+    console.log('body', request.body)
+    console.log('params', request.params)
+    console.log('query', request.query)
+    console.log('headers', request.headers)
+    console.log('ip', request.ip)
     return this.appService.getHello();
   }
 
