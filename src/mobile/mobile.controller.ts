9575cc2defc90beb6d480c4f3d51076fce067c8b
Юрий Аушев <wydo17@yandex.ua>
2021-04-05 16:41:51 +0300

    day 4_2

--- src/mobile/mobile.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller({ host: `mobile.${process.env.APP_HOST}` })
-export class MobileController {
-  @Get()
-  index(): string {
-    return 'mobile version';
-  }
-}
