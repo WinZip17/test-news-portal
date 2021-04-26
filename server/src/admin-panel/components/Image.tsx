f288412a60399273220405b6ea68f7407ad0e83b
Юрий Аушев <wydo17@yandex.ua>
2021-04-26 17:04:19 +0300

    upd AdminBro

--- server/src/admin-panel/components/Image.tsx
+++ server/src/admin-panel/components/Image.tsx
@@ -1,14 +1,47 @@
 const React = require('react');
 
+// import {
+//   BasePropertyProps,
+//   Box,
+//   DropZone,
+//   Label,
+//   OnDropZoneChange,
+// } from 'admin-bro';
+
+const adminBro = require('admin-bro');
 const Image = (props): JSX.Element => {
   console.log('props', props);
   console.log('props.record.params.image', props.record.params.image);
+
+  const { property, record, onChange } = props;
+  //
+  // const onUpload = (files: File[]) => {
+  //   const newRecord = { ...record };
+  //   onChange({
+  //     ...newRecord,
+  //     params: {
+  //       ...newRecord.params,
+  //       [property.name]: files,
+  //     },
+  //   });
+  //   event.preventDefault();
+  // };
+
   return (
-    <div style={{ marginBottom: 15 }}>
-      <h3>Image</h3>
-      <img src={`http://localhost:3003/${props.record.params.image}`} alt="" />
-    </div>
-  );
+    <div>fjrkifjrei</div>
+  )
+  // return (
+  //   <Box>
+  //     <Label>{property.label}</Label>
+  //     <img src={`http://localhost:3003/${record.params.image}`} alt="" />
+  //
+  //     <DropZone
+  //       multiple
+  //       onChange={onUpload}
+  //       validate={{ mimeTypes: ['image/png', 'image/jpg', 'image/jpeg'] }}
+  //     />
+  //   </Box>
+  // );
 };
 
 export default Image;
