847c5f139477be774b899df3b17536da75232f79
Юрий Аушев <wydo17@yandex.ua>
2021-05-04 15:07:27 +0300

    upd

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: 'news-portal-app',
+      script: 'server/dist/main.js',
+      watch: '.',
+    },
+    //   {
+    //   script: './service-worker/',
+    //   watch: ['./service-worker']
+    // }
+  ],
+
+  deploy: {
+    production: {
+      user: 'reactness',
+      host: '6rs.ru',
+      ref: 'origin/master',
+      repo: 'https://github.com/WinZip17/test-news-portal.git',
+      path: '/data/reactness/app',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'cd client && npm install && cd .. && cd server && npm install && cd .. && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
