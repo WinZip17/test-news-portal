2f2abc46d409ed3a23c913c2fa433fabf99ba47a
Юрий Аушев <wydo17@yandex.ua>
2021-05-04 15:34:09 +0300

    upd

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -17,10 +17,10 @@ module.exports = {
       host: '6rs.ru',
       ref: 'origin/master',
       repo: 'https://github.com/WinZip17/test-news-portal.git',
-      path: '/var/www/app',
+      path: '/data/reactness/app',
       'pre-deploy-local': '',
       'post-deploy':
-        'cd client && npm install && cd .. && cd server && npm install && cd .. && pm2 reload ecosystem.config.js --env production',
+        'npm install && npm run-script build && cd .. && cd client && npm install && npm run-script build && cd .. && pm2 startOrRestart ecosystem.config.js --env production',
       'pre-setup': '',
     },
   },
