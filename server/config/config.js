5bff74aae7bb7c4b3c393cdb9d687db37481de8e
Юрий Аушев <wydo17@yandex.ua>
2021-05-05 12:14:10 +0300

    fix bd config

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   development: {
+    dialect: 'postgres',
     username: process.env.DEV_DB_USER || 'postgres',
     password: process.env.DEV_DB_PASSWORD || 'postgres',
     database: process.env.DEV_DB_NAME || 'news_db',
     host: process.env.DEV_DB_HOST || 'localhost',
     port: Number(process.env.DEV_DB_PORT) || 5432,
-    dialect: process.env.DEV_DB_DIALECT || 'postgres',
     dialectOptions: {
       bigNumberStrings: true,
     },
   },
   production: {
+    dialect: 'postgres',
     adapter: process.env.PROD_DB_ADAPTER,
     username: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOST,
     port: Number(process.env.PROD_DB_PORT),
-    dialect: process.env.PROD_DB_DIALECT,
     dialectOptions: {
       bigNumberStrings: true,
     },
