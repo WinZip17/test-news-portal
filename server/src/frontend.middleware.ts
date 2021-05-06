17d77f353106e0010acad54ca6daddef6dc70b43
Юрий Аушев <wydo17@yandex.ua>
2021-05-06 08:51:16 +0300

    test upd

--- server/src/frontend.middleware.ts
+++ server/src/frontend.middleware.ts
@@ -6,11 +6,11 @@ const resolvePath = (file: string) => path.resolve(`./dist/${file}`);
 export class FrontendMiddleware implements NestMiddleware {
   use(req, res, next) {
     const { baseUrl } = req;
-    if (baseUrl.indexOf('/api') === 0) {
-      console.log('api');
+    if (baseUrl.indexOf('/api') === 0 || baseUrl.indexOf('admin') === 0) {
+      console.log('next');
       next();
     } else {
-      console.log('no api', baseUrl);
+      console.log('no next', baseUrl);
       if (req.headers.accept.includes('html')) {
         res.sendFile(resolvePath('index.html'));
       } else {
