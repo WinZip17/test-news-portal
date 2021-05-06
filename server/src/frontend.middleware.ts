8e254066b5a502731837a99488cab3f245129e2e
Юрий Аушев <wydo17@yandex.ua>
2021-05-06 08:24:51 +0300

    test upd

--- server/src/frontend.middleware.ts
+++ server/src/frontend.middleware.ts
@@ -7,8 +7,10 @@ export class FrontendMiddleware implements NestMiddleware {
   use(req, res, next) {
     const { baseUrl } = req;
     if (baseUrl.indexOf('/api') === 0) {
+      console.log('api');
       next();
     } else {
+      console.log('no api', baseUrl);
       if (req.headers.accept.includes('html')) {
         res.sendFile(resolvePath('index.html'));
       } else {
