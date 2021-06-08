c4868bcf05e657143525bcc394677ca5704156e2
Юрий Аушев <wydo17@yandex.ua>
2021-06-08 13:38:29 +0300

    upd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       path: '/data/reactness/app',
       'pre-deploy-local': '',
       'post-deploy':
-        'cd server && yarn install && yarn build && ' +
+        'NODE_ENV=production && cd server && yarn install && yarn build && ' +
         'npx sequelize-cli db:migrate && cd .. && ' +
         'cd client && yarn install && yarn build && cd .. && ' +
         'pm2 startOrReload ecosystem.config.js --env production',
